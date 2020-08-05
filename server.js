b354e1c9de73f66ea64a2c9a809828ac00539181
Wilson Ruan (ACICA) <Wilson_Ruan@aci.corpnet.asus>
2020-08-05 11:24:16 -0400

    Inits server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// Sets up the Express App and Node.js
+var express = require("express");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
